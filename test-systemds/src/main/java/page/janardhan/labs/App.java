210e9c1c72e936b4f36355c80c7e915f037c802f
Janardhan Pulivarthi <j143@protonmail.com>
2021-06-12 08:25:50 +0000

    Initial commit

---
+++ test-systemds/src/main/java/page/janardhan/labs/App.java
@@ -0,0 +1,13 @@
+package page.janardhan.labs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
