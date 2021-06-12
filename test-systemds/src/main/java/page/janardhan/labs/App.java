c6414b07c6cf5a122206c23aebf88f1c7d3b9bc2
Janardhan Pulivarthi <j143@protonmail.com>
2021-06-12 09:42:33 +0000

    add systemds pom and mlcontext script

--- test-systemds/src/main/java/page/janardhan/labs/App.java
+++ test-systemds/src/main/java/page/janardhan/labs/App.java
@@ -1,5 +1,8 @@
 package page.janardhan.labs;
 
+import static org.apache.sysds.api.mlcontext.ScriptFactory.dmlFromUrl;
+import org.apache.sysds.api.mlcontext.Script;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        String urlString = "https://raw.githubusercontent.com/apache/systemml/master/src/test/scripts/applications/hits/HITS.dml";
+        Script script = dmlFromUrl(urlString);
+        String s = script.getScriptString();
+        System.out.println(s);
     }
 }
